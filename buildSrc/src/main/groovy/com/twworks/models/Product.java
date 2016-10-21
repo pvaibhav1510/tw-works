2360b5e65a3acc8aef4455b84fe7ca7178b5c407
vaibhav1510 <vaibhav@chargebee.com>
2016-10-21 10:45:11 +0530

    product

---
+++ buildSrc/src/main/groovy/com/twworks/models/Product.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.twworks.models;
+
+/**
+ *
+ * @author cb-vaibhav
+ */
+public class Product {
+
+    private String name;
+    private String description;
+    private int measure;
+    private float price;
+    private String unit;
+
+}
